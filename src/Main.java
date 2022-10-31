84ea5d6e39b04599c80147fd4c4a7cf2501a0529
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-10-31 08:17:38 +0300

    Hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        System.out.println(multiplicationTable);
     }
 }
